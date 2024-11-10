d32fec930f1af461519332e1eb2b55ee25f16520
99choco99 <99choco99@naver.com>
2024-11-10 22:48:59 +0900

    241110 로직없음

--- Assets/Scripts/HUD_Scripts/HUD.cs
+++ Assets/Scripts/HUD_Scripts/HUD.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class HUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum InfoType { time, score, hp};
+    InfoType type;
+
+
+    private void LateUpdate()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
