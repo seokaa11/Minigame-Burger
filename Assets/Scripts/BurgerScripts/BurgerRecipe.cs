0af501ea9582cfd02cdc07f41a93253068e20e46
99choco99 <99choco99@naver.com>
2025-02-06 02:33:37 +0900

    버거제출 일부구현
    Cats Burger 까진 구현

---
+++ Assets/Scripts/BurgerScripts/BurgerRecipe.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/BurgerRecipe", fileName = "BurgerRecipe")]
+public class BurgerRecipe : ScriptableObject
+{
+    public int BurgerNum; //버거 번호
+    public List<GameObject> Ingredients; //버거 재료
+}
---
+++ Assets/Scripts/HUD_Scripts/EvalueateBurger.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EvalueateBurger : MonoBehaviour
+{
+    public Scoredata [] scoredata;
+    public BurgerRecipe[] burgerRecipes;
+    public GameObject submitedBurger;
+    public int requestBurgerNum;
+
+
+    //버튼이 눌리면 평가
+    public void OnEvalue()
+    {
+        BurgerScore(submitedBurger, requestBurgerNum);
+    }
+
+    // 버거 상태에 따라 점수가 주어지고 대화문 출력
+    public void BurgerScore(GameObject burger,int requestBurgerNum)
+    {
+        for (int i = 0; i < scoredata.Length / 2; i++)
+        {
+            if (GameManager.instance.takenTime > 20) { i = 4; }
+            if (scoredata[i].Time >= GameManager.instance.takenTime)
+            {
+                if (IsPerferctBurger(burger, burgerRecipes[requestBurgerNum]) && i != 4) { i += 5; }
+                GameManager.instance.score += scoredata[i].score;
+                GameManager.instance.health += scoredata[i].health;
+                GameManager.instance.dialog = scoredata[i].dialog;
+                GameManager.instance.takenTime = 0;
+                
+                break;
+            }
+        }
+    }
+
+    //버거가 완벽한지 판단
+    public bool IsPerferctBurger(GameObject items, BurgerRecipe recipe)
+    {
+        for (int i = 0; i < recipe.Ingredients.Count; i++)
+        {
+            if (items.transform.childCount != recipe.Ingredients.Count) { return false; }
+            else if (recipe.Ingredients[i].transform.name != items.transform.GetChild(i).name) { return false; }
+        }
+        return true;
+    }
+}
