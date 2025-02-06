81ce21c68bb0106b9a928075601a54b11c3d2bef
99choco99 <99choco99@naver.com>
2025-02-06 15:28:24 +0900

    버그 약 수정
    재시작시 버거 제출 버그 있음

--- Assets/Scripts/MainScene/GameManager.cs
+++ Assets/Scripts/MainScene/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
         takenTime += Time.deltaTime;
         if (health <= 0 && !isGameOver)
         {
-            GameOverPanel.SetActive(true);
+            Instantiate(GameOverPanel);
             endGame();
         }
     }
